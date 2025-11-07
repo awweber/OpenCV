3a4c4a87001a638ae2afc9a0784121247ad8365b
Alexander Weber <awweber197@gmail.com>
2025-11-07 21:19:41 +0100

    CleanUp: Keine Downloads mehr in tutorial und Face_Detection.py

--- Face_Detection.py
+++ Face_Detection.py
@@ -4,36 +4,7 @@ import sys
 from zipfile import ZipFile
 from urllib.request import urlretrieve
 
-
-# ========================-Downloading Assets-========================
-def download_and_unzip(url, save_path):
-    print(f"Downloading and extracting assests....", end="")
-
-    # Downloading zip file using urllib package.
-    urlretrieve(url, save_path)
-
-    try:
-        # Extracting zip file using the zipfile package.
-        with ZipFile(save_path) as z:
-            # Extract ZIP file contents in the same directory.
-            z.extractall(os.path.split(save_path)[0])
-
-        print("Done")
-
-    except Exception as e:
-        print("\nInvalid file.", e)
-
-
-URL = r"https://www.dropbox.com/s/efitgt363ada95a/opencv_bootcamp_assets_12.zip?dl=1"
-
-asset_zip_path = os.path.join(os.getcwd(), f"data/opencv_bootcamp_assets_12.zip")
-
-# Download if assest ZIP does not exists.
-if not os.path.exists(asset_zip_path):
-    download_and_unzip(URL, asset_zip_path)
-# ====================================================================
-
-
+# Initialize video source
 s = 0
 if len(sys.argv) > 1:
     s = sys.argv[1]
